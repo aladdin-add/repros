70ccbc2c564fbc4cb849726351022b64221badda
唯然 <weiran.zsd@outlook.com>
2023-08-10 14:21:10 +0800

    create repro

---
+++ README.md
@@ -0,0 +1,8 @@
+# vite issue repro 
+
+https://github.com/vitejs/vite/discussions/14062
+
+```bash
+$ npm install
+$ npm run dev
+```
---
+++ main.js
@@ -0,0 +1,5 @@
+import foo from 'foo/mod.js'
+import { Button } from 'antd-mobile'
+
+alert('success', foo)
+alert('antd-mobile', Button)
